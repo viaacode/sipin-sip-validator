7fa277534b68b6ef19f95f44ab6de399a8fb32e3
Mattias.Poppe <poppe.mattias@gmail.com>
2023-01-17 16:14:36 +0100

    Initial commit

---
+++ app/app.py
@@ -0,0 +1,6 @@
+class EventListener:
+    def __init__(self):
+        pass
+
+    def main(self):
+        pass
---
+++ main.py
@@ -0,0 +1,4 @@
+from app.app import EventListener
+
+if __name__ == "__main__":
+    EventListener().main()
